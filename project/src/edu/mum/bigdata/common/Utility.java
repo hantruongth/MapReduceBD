912e7ae9560b19394cac705562bbb3cc2bc4a73b
Thang Mai <mhthang.it@gmail.com>
2019-11-14 00:00:14 -0600

    add utility class

--- project/src/edu/mum/bigdata/common/Utility.java
+++ project/src/edu/mum/bigdata/common/Utility.java
@@ -1,4 +1,14 @@
 package edu.mum.bigdata.common;
 
-public class Utility {
+public class Utility
+{
+    public static int toNumber(String strNum) {
+        int d = 0;
+        try {
+            d = Integer.parseInt(strNum);
+        } catch (NumberFormatException | NullPointerException nfe) {
+            return 0;
+        }
+        return d;
+    }
 }
